cd1ef6219d78d8be9dce342ccd295a21191bb1c8
Davedaster <166388039+Davedaster@users.noreply.github.com>
2026-06-28 12:52:43 +0100

    Use browser-safe ETA formatter on tracking page

--- app/routes/apps.track.$routeId.tsx
+++ app/routes/apps.track.$routeId.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 import { CustomerEtaConfidenceCard } from "../components/CustomerEtaConfidenceCard";
 import { CustomerSupportCard } from "../components/CustomerSupportCard";
-import { formatEtaSlot } from "../lib/etaSlots.server";
+import { formatEtaSlot } from "../lib/etaSlots";
 import { getCustomerTracking } from "../lib/tracking.server";
 
 const TRACKING_REFRESHED_KEY = "routeBuddyTrackingRefreshed";
