c8420e07734819db8f1cbfd9ac6586e29445608f
Davedaster <166388039+Davedaster@users.noreply.github.com>
2026-07-02 10:52:10 +0100

    Allow public TomTom key lookup

--- app/routes/api.tomtom-key.tsx
+++ app/routes/api.tomtom-key.tsx
@@ -2,11 +2,8 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 import { getAppCredentials } from "../lib/appCredentials.server";
-import { authenticate } from "../shopify.server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
-
+export const loader = async (_args: LoaderFunctionArgs) => {
   const credentials = await getAppCredentials();
 
   return json({ apiKey: credentials.tomtomApiKey || "" });
