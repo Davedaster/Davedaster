09f99ec08466c9d2994356833d9d4cbffb023811
Davedaster <166388039+Davedaster@users.noreply.github.com>
2026-07-02 13:57:16 +0100

    Expose driver completion popup template

--- app/routes/api.tomtom-key.tsx
+++ app/routes/api.tomtom-key.tsx
@@ -2,9 +2,16 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 import { getAppCredentials } from "../lib/appCredentials.server";
+import { getDriverCompletionMessageTemplate } from "../lib/driverCompletionSettings.server";
 
 export const loader = async (_args: LoaderFunctionArgs) => {
-  const credentials = await getAppCredentials();
+  const [credentials, driverCompletionMessageTemplate] = await Promise.all([
+    getAppCredentials(),
+    getDriverCompletionMessageTemplate(),
+  ]);
 
-  return json({ apiKey: credentials.tomtomApiKey || "" });
+  return json({
+    apiKey: credentials.tomtomApiKey || "",
+    driverCompletionMessageTemplate,
+  });
 };
