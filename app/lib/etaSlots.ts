73df362c5023bc7ef0508b90d34a87b029d0bf3d
Davedaster <166388039+Davedaster@users.noreply.github.com>
2026-07-02 12:00:02 +0100

    Format ETA slots in UK time

--- app/lib/etaSlots.ts
+++ app/lib/etaSlots.ts
@@ -2,6 +2,7 @@ export function formatEtaSlot(start: Date, end: Date) {
   const formatter = new Intl.DateTimeFormat("en-GB", {
     hour: "2-digit",
     minute: "2-digit",
+    timeZone: "Europe/London",
   });
 
   return `${formatter.format(start)} to ${formatter.format(end)}`;
