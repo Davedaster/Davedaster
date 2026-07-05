b47bc11a3f4be1269d8da82fc49dc966ca83a1ab
Davedaster <166388039+Davedaster@users.noreply.github.com>
2026-07-05 12:03:36 +0100

    Mark area filters as read only

--- app/routes/app.area-filters.tsx
+++ app/routes/app.area-filters.tsx
@@ -15,60 +15,24 @@ export default function AreaFilters() {
         <Layout.Section>
           <LegacyCard sectioned>
             <FormLayout>
-              <TextField
-                label="Cornwall"
-                value="TR, PL"
-                autoComplete="off"
-                helpText="Postcode prefixes for Cornwall"
-              />
-              <TextField
-                label="South West"
-                value="EX, TQ, TA, BS, BA"
-                autoComplete="off"
-                helpText="Postcode prefixes for South West"
-              />
-              <TextField
-                label="Wales"
-                value="CF, NP, SA, LL, SY, LD"
-                autoComplete="off"
-                helpText="Postcode prefixes for Wales"
-              />
-              <TextField
-                label="South East"
-                value="BN, RH, TN, ME, CT, SS, CM, CO"
-                autoComplete="off"
-                helpText="Postcode prefixes for South East"
-              />
-              <TextField
-                label="South"
-                value="SO, PO, BH, DT, SP, RG"
-                autoComplete="off"
-                helpText="Postcode prefixes for South"
-              />
-              <TextField
-                label="Central"
-                value="GL, OX, MK, NN, HR, WR, DY, B, CV"
-                autoComplete="off"
-                helpText="Postcode prefixes for Central"
-              />
-              <TextField
-                label="Northern"
-                value="DE, NG, LN, ST, TF, WV, WS, LE"
-                autoComplete="off"
-                helpText="Postcode prefixes for Northern"
-              />
+              <Text as="p" variant="bodyMd" tone="subdued">
+                These area filters are currently read only. They show the postcode groups used for planning.
+              </Text>
+              <TextField label="Cornwall" value="TR, PL" autoComplete="off" readOnly helpText="Postcode prefixes for Cornwall" />
+              <TextField label="South West" value="EX, TQ, TA, BS, BA" autoComplete="off" readOnly helpText="Postcode prefixes for South West" />
+              <TextField label="Wales" value="CF, NP, SA, LL, SY, LD" autoComplete="off" readOnly helpText="Postcode prefixes for Wales" />
+              <TextField label="South East" value="BN, RH, TN, ME, CT, SS, CM, CO" autoComplete="off" readOnly helpText="Postcode prefixes for South East" />
+              <TextField label="South" value="SO, PO, BH, DT, SP, RG" autoComplete="off" readOnly helpText="Postcode prefixes for South" />
+              <TextField label="Central" value="GL, OX, MK, NN, HR, WR, DY, B, CV" autoComplete="off" readOnly helpText="Postcode prefixes for Central" />
+              <TextField label="Northern" value="DE, NG, LN, ST, TF, WV, WS, LE" autoComplete="off" readOnly helpText="Postcode prefixes for Northern" />
             </FormLayout>
           </LegacyCard>
         </Layout.Section>
         <Layout.Section variant="oneThird">
           <LegacyCard title="About Area Filters" sectioned>
             <BlockStack gap="200">
-              <Text as="p" variant="bodyMd">
-                Area filters help you group orders by their postcode prefixes.
-              </Text>
-              <Text as="p" variant="bodyMd">
-                You can add multiple prefixes separated by commas.
-              </Text>
+              <Text as="p" variant="bodyMd">Area filters help group orders by postcode prefixes.</Text>
+              <Text as="p" variant="bodyMd">Saving custom filters has not been added yet, so these fields are locked to prevent confusion.</Text>
             </BlockStack>
           </LegacyCard>
         </Layout.Section>
